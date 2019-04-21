7a7d8802476c00c32ccd6ef2082f3f3a5dd0c4fb
Nikita-Elkin <nikita.elkin79@gmail.com>
2019-04-20 19:56:56 -0500

    Finished gps tracking

--- app/src/main/java/com/example/nikitaelkin/spurapp/main.java
+++ app/src/main/java/com/example/nikitaelkin/spurapp/main.java
@@ -1,13 +1,107 @@
 package com.example.nikitaelkin.spurapp;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class main extends AppCompatActivity {
+import com.google.android.gms.location.DetectedActivity;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.nlopez.smartlocation.OnLocationUpdatedListener;
+import io.nlopez.smartlocation.OnReverseGeocodingListener;
+import io.nlopez.smartlocation.SmartLocation;
+import io.nlopez.smartlocation.location.providers.LocationGooglePlayServicesProvider;
+
+public class main extends AppCompatActivity implements OnLocationUpdatedListener {
+
+    private LocationGooglePlayServicesProvider provider;
+
+    private static final int LOCATION_PERMISSION_ID = 1001;
+    private TextView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        test = findViewById(R.id.textView2);
+        Button startLocation = (Button) findViewById(R.id.button);
+        startLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Location permission not granted
+                if (ContextCompat.checkSelfPermission(main.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(main.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_ID);
+                    return;
+                }
+                startLocation();
+            }
+        });
+
+
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == LOCATION_PERMISSION_ID && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            startLocation();
+        }
+    }
+
+    private void showLast() {
+        Location lastLocation = SmartLocation.with(this).location().getLastLocation();
+        if (lastLocation != null) {
+            test.setText(
+                    String.format("[From Cache] Latitude %.6f, Longitude %.6f",
+                            lastLocation.getLatitude(),
+                            lastLocation.getLongitude())
+            );
+        }
+
+    }
+
+    private void startLocation() {
+
+        provider = new LocationGooglePlayServicesProvider();
+        provider.setCheckLocationSettings(true);
+
+        SmartLocation smartLocation = new SmartLocation.Builder(this).logging(true).build();
+
+        smartLocation.location(provider).start(this);
+
+    }
+
+
+    private void showLocation(Location location) {
+        if (location != null) {
+            final String text = String.format("Latitude %.6f, Longitude %.6f",
+                    location.getLatitude(),
+                    location.getLongitude());
+           test.setText(text);
+        } else {
+            test.setText("Null location");
+        }
+    }
+
+    @Override
+    public void onLocationUpdated(Location location) {
+        showLocation(location);
+    }
+
 }
